5824a559eb2bb1927f512f63cbaef940c1888c91
Raisul <raisulraisul524@gmail.com>
2026-08-14 01:47:42 +0530

    Initial SmartCare Hospital Spring Boot project

---
+++ src/main/java/smartcarehospital/smartcarehospital/SmartcareHospitalApplication.java
@@ -0,0 +1,13 @@
+package smartcarehospital.smartcarehospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcareHospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartcareHospitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/smartcarehospital/smartcarehospital/SmartcareHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package smartcarehospital.smartcarehospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcareHospitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
